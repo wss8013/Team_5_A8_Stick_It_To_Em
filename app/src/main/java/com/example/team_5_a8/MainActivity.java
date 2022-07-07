f4ba3f8d297ea407fdcd01139962ece083fa29e6
stellashen0911 <minjieshen0911@gmail.com>
2022-07-07 10:50:56 -0700

    Initial Commit Test

---
+++ app/src/main/java/com/example/team_5_a8/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.team_5_a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("test git");
+    }
+}
