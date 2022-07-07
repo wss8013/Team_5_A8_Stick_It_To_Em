a67ebdc5a996aca036a581c6ee40e5ce169053a3
stellashen0911 <minjieshen0911@gmail.com>
2022-07-07 15:36:00 -0700

    Add user registration feature and link the data to update in the database

--- app/src/main/java/com/example/team_5_a8/MainActivity.java
+++ app/src/main/java/com/example/team_5_a8/MainActivity.java
@@ -1,16 +1,68 @@
 package com.example.team_5_a8;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.provider.Settings.Secure;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference myDataBase;
+    private FirebaseAuth mAuth;
+    private EditText userNameInput;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test git");
-        System.out.println("test git2");
+
+        //set up all the UI view elements
+        userNameInput = findViewById(R.id.editTextTextPersonName);
+        registerBtn = findViewById(R.id.button_register);
+
+        myDataBase = FirebaseDatabase.getInstance().getReference();
+
+
+        // Set on Click Listener on register button
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                registerUserAccount();
+            }
+        });
+    }
+
+    public void registerUserAccount() {
+        //get the user input username data
+        String userName;
+        userName = userNameInput.getText().toString();
+
+        //get the current device id
+        String android_id = Secure.getString(
+                getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
+
+        //validate for the user name input
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter a valid user name",  Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //create the new user and upload it to the database
+        User user = new User(userName, android_id);
+        myDataBase.child("user").setValue(user);
+
     }
 }
---
+++ app/src/main/java/com/example/team_5_a8/User.java
@@ -0,0 +1,11 @@
+package com.example.team_5_a8;
+
+public class User {
+    public String username;
+    public String userDeviceID;
+
+    public User(String username, String userDeviceID) {
+        this.username = username;
+        this.userDeviceID = userDeviceID;
+    }
+}
